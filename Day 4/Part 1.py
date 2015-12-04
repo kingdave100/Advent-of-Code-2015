4d2d7bf6e2831a850be2b7fedf2001ac78b9ce3a
Dave <dave@davek.ca>
2015-12-04 00:28:26 -0500

    Day 4

---
+++ Day 4/Part 1.py
@@ -0,0 +1,11 @@
+import md5
+x = 0
+while True:
+    x += 1
+    m = md5.new()
+    m.update("yzbqklnj"+str(x))
+    if m.hexdigest()[0:5] == "00000":
+        print "yzbqklnj"+str(x)
+        print m.hexdigest()
+        print x
+        break
