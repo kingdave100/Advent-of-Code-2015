184418e73e7bf16ec1834a9a76004b6bf9a02c87
Dave <dave@davek.ca>
2015-12-10 00:21:11 -0500

    Day 10

---
+++ Day 10/Part 2.py
@@ -0,0 +1,19 @@
+input = "1113122113"
+def lookAndSay(num):
+    output = ""
+    repeat = num[0]
+    number = num[1:]+" "
+    times = 1
+
+    for i in number:
+        if i != repeat:
+            output += str(times)+repeat
+            times = 1
+            repeat = i
+        else:
+            times += 1
+    return output
+
+for i in range(50):
+    input = lookAndSay(input)
+print len(input)
