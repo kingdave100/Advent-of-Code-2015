74a79734ce99601a5df35a7bbc466926f8c2b60a
Dave <dave@davek.ca>
2015-12-11 22:37:05 -0500

    Day 11

---
+++ Day 11/Part 1.py
@@ -0,0 +1,32 @@
+import re
+password = 'hxbxxyzz'
+
+def validity(s):
+    for c in ['i', 'l', 'o']:
+        if c in s:
+            return False
+
+    for i in range(0, len(s)-2):
+        if ord(s[i]) == ord(s[i+1]) - 1 and ord(s[i+1]) == ord(s[i+2]) - 1:
+            break
+    else:
+        return False
+
+    if len(re.findall(r'(.)\1+', s)) < 2:
+        return False
+
+    return True
+
+def increment(s):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+    try:
+        return s[0:-1] + alpha[alpha.index(s[-1]) + 1]
+    except IndexError:
+        return increment(s[0:-1]) + 'a'
+
+while True:
+    password = increment(password)
+    if validity(password):
+        print password
+        break
