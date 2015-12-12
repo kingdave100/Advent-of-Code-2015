e057d6861ede4fa9fc184257b548993d13784b06
Dave <dave@davek.ca>
2015-12-12 00:37:53 -0500

    Cleanup

--- Day 12/Part 1.py
+++ Day 12/Part 1.py
@@ -1,6 +1,3 @@
 import re
 input = f = open('input.txt', 'r').read()
-print eval('+'.join(c for c in input if c.isdigit() or c == "-"))
-
-print '+'.join(re.findall(r'\-?\d+', input))
 print eval('+'.join(re.findall(r'\-?\d+', input)))
